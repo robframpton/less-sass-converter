e7b5fc41ed713f7d195f62c58ab8ecae5d4dcdd0
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-09-23 13:02:41 -0700

    Discarding unnecessary gulp actions

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,11 @@ var gulp = require('gulp');
 var patterns = require('./lib/patterns');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
-var rimraf = require('gulp-rimraf');
 
 gulp.task(
     'convert',
     function() {
-        var less = gulp.src('convert/*.less')
-            .pipe(gulp.dest('build'))
-            .pipe(rimraf());
+        var less = gulp.src('convert/*.less');
 
         for (var i = 0; i < patterns.length; i++) {
             var pattern = patterns[i];
