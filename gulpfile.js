1e00ddb9397dd84645dc989fbdd141c726ce3940
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-10-10 11:03:04 -0700

    Formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var requireDir = require('require-dir');
-var tasksDir = requireDir('./tasks');
 var gulp = require('gulp');
-
+var requireDir = require('require-dir');
 var run = require('run-sequence');
+
+var tasksDir = requireDir('./tasks');
 
 gulp.task('build', function(callback) {
     run('convert', 'rename', 'compile-sass', callback);
