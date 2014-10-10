69b120773ec50694c120a1b52523d052188d81cc
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-10-10 12:43:30 -0700

    Clean build directory of scss files before convert task

--- tasks/convert.js
+++ tasks/convert.js
@@ -2,9 +2,19 @@ var gulp = require('gulp');
 var patterns = require('../lib/patterns');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace-task');
+var rimraf = require('gulp-rimraf');
+
+gulp.task(
+    'clean',
+    function () {
+        return gulp.src('build/*.scss', { read: false })
+            .pipe(rimraf());
+    }
+);
 
 gulp.task(
     'convert',
+    ['clean'],
     function() {
         return gulp.src('convert/*.less')
             .pipe(replace({ patterns: patterns }))
